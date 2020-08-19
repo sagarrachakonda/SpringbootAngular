cab0f489d62b8236a3b54d324fb51eb305de8361
sagar <saagar.rachakonda@gmail.com>
2020-08-19 11:11:42 +0530

    ApringbootAngular
    AngularSpringbootCrud

---
+++ UserPortalUsingAngularSpringBoot/README.md
@@ -0,0 +1,14 @@
+# spring-boot-angular5 practice by sagar
+This is a sample project using Angular 5 and spring Boot.Spring boot has been used to expose REST Endpoints and the client is written in angular. 
+This is a complete web application with a connectivity to postgres database. We have used spring data to connect to the database and perform CRUD
+operation.
+
+The application has CRUD operation example for user entity. Here, you can create
+a user, read users record and also delete user.
+
+This project uses following versions:
+
+1. Spring Boot v1.5.9
+2. Angular v5.0.0
+3. Maven
+
---
+++ UserPortalUsingAngularSpringBoot/user-portal/src/main/java/com/sagar/userportal/User.java
@@ -0,0 +1,53 @@
+package com.sagar.userportal;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
